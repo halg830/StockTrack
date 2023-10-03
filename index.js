4bab1ad12f6f02029d4efc3a557a43a3522e3ab0
Jeferson <jeffersonitjr@gmail.com>
2023-10-03 13:21:55 -0500

    index.js y sockets

---
+++ index.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import "dotenv/config"
+import mongoose from 'mongoose'
+import http from 'http';
+import * as io from 'socket.io'
+import controllerSockets from "./sockets/controller.js"
+
+const port=process.env.PORT
+let app = express();
+app.use(express.json());
+app.use(express.static('public'))
+const server = http.createServer(app)
+
+let ioServer = new io.Server(server);
+app.set('socketio', io)
+
+ioServer.on('connection', controllerSockets);
+
+mongoose.connect(`${process.env.DB}`)
+  .then(() => console.log('Connected!'));
+
+server.listen(port, () => {
+    console.log(`Servidor corriendo en el puerto ${port}`);
+});   
