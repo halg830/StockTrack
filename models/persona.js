8dcce6764385bbfcc9b049aca50f05861ee06bf3
unknown <SENA@SGLDFPCATSD254.FormacionSena.RED>
2023-10-03 15:34:46 -0500

    Registross

--- models/persona.js
+++ models/persona.js
@@ -3,9 +3,9 @@ import mongoose from "mongoose";
 const personaSchema = new mongoose.Schema({
     nombre: { type: String, minlenght: 5, require:true },
     identificacion: { type: String, require:true, unique:true },
-    nombreUsuario: { type: String, require:true},
+    nombreUsuario: { type: String, require:true, unique:true},
     correo: { type: String, require:true},
-    telefono: {type: String, require:true},
+    telefono: { type: String, required: true, validate: /^\d{10}$/ },
     codigoRol : {type:String, require:true},
     contraseña : {type:String, require:true},
     createAT : {type:Date,default: Date.now },
--- routes/persona.js
+++ routes/persona.js
@@ -14,7 +14,13 @@ router.post('/registro',[
     check("correo", "Digite el correo").isEmail(),
     check("telefono", "Digite el telefono").not().isEmpty(),
     check("codigoRol", "Digite el codigo del rol").not().isEmpty(),
-    check("contraseña", "Digite la contraseña").not().isEmpty(),
+    check("contraseña", "La contraseña debe contener al menos 1 mayúscula, 1 minúscula, al menos 2 números y un carácter especial").custom((value) => {
+        const vali = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d.*\d)(?=.*[@#$%^&+=!]).{8,}$/;
+        if (!vali.test(value)) {
+            throw new Error("La contraseña no cumple con los requisitos.");
+        }
+        return true;
+    }),
     validarCampos
 ],httpPersona.registroVendedor)
 
