39e878f71f198acdf1b217f91496684bdc73c8ac
halg830 <halg830@gmail.com>
2024-02-02 17:59:53 -0500

    ajustes en cambio de contraseña usuario

--- controllers/usuario.js
+++ controllers/usuario.js
@@ -76,26 +76,25 @@ const httpUsuario = {
         pass: process.env.password,
       },
     });
-  
-    // Contenido del correo electrónico
+
     const mailOptions = {
       from: process.env.userEmail,
       to: correo,
       subject: 'Recuperación de Contraseña',
-      text: 'Haz clic en el siguiente enlace para restablecer tu contraseña: http://tu-app.com/reset-password?token=TU_TOKEN',
+      text: 'Haz clic en el siguiente enlace para restablecer tu contraseña: http://localhost:5173/#/recuperar-password?token=' + token,
     };
   
-    // Envía el correo electrónico
     transporter.sendMail(mailOptions, (error, info) => {
       if (error) {
         console.error(error);
-        res.status(500).json({ success: false, message: 'Error al enviar el correo electrónico.' });
+        res.status(500).json({ success: false, msg: 'Error al enviar el correo electrónico.' });
       } else {
         console.log('Correo electrónico enviado: ' + info.response);
-        res.json({ success: true, message: 'Correo electrónico enviado con éxito.' });
+        res.json({ success: true, msg: 'Correo electrónico enviado con éxito.' });
       }
     });
   },
+
   putCambioPassword: async (req, res) => {
     const { id } = req.params;
     const { password, newPassword } = req.body;
@@ -107,25 +106,25 @@ const httpUsuario = {
         return res.status(404).json({ error: "Usuario no encontrado" });
       }
 
-      const validPassword = bcryptjs.compareSync(password, usuario.password);
+      const passAnterior = usuario.password
+
+      const validPassword = bcryptjs.compareSync(String(password), String(passAnterior));
 
       if (!validPassword) {
-        return res
-          .status(400)
-          .json({ error: "Contraseña actual incorrecta" });
+        return res.status(401).json({ error: "Contraseña actual incorrecta" });
       }
 
       const salt = bcryptjs.genSaltSync();
       const cryptNewPassword = bcryptjs.hashSync(newPassword, salt);
 
-      usuario.password = cryptNewPassword;
-      await usuario.save();
+      await Usuario.findByIdAndUpdate(usuario.id, { password:cryptNewPassword}, {new: true})
 
       return res
         .status(200)
-        .json({ mensaje: "Contraseña actualizada con éxito" });
+        .json({ msg: "Contraseña actualizada con éxito" });
     } catch (error) {
-      return res.status(500).json({ error: "Error interno del servidor" });
+      console.log(error);
+      return res.status(500).json({ msgError: "Error interno del servidor", error });
     }
   },
   logOut: async (req, res) => {
