6e274f03957d189ee0e193f9883202561a0866b3
halg830 <halg830@gmail.com>
2024-02-13 11:07:24 -0500

    editar usuario

--- routes/usuario.js
+++ routes/usuario.js
@@ -1,13 +1,19 @@
-import { Router } from "express"
+import { Router } from "express";
 import httpUsuario from "../controllers/usuario.js";
 import { check } from "express-validator";
-import validarCampos from "../middlewares/validar.js"
+import validarCampos from "../middlewares/validar.js";
 import { validarJWT } from "../middlewares/validar-jwt.js";
 import helpersUsuario from "../helpers/usuario.js";
 
-const router=new Router()
+const router = new Router();
 
-router.post('/registro',[
+//Get
+router.get("/all", [validarJWT], httpUsuario.getAll);
+
+//Post
+router.post(
+  "/registro",
+  [
     validarJWT,
     check("nombre", "Digite el nombre").not().isEmpty(),
     check("identificacion", "Digite la identificacion").not().isEmpty(),
@@ -15,31 +21,62 @@ router.post('/registro',[
     check("correo", "Digite el correo").isEmail(),
     check("telefono", "Digite el telefono").not().isEmpty(),
     check("rol", "Digite el codigo del rol").not().isEmpty(),
-    check("rol", "Rol no válido: admin, instructor, bodega").custom(helpersUsuario.validarRol),
-    check("password", "La contraseña debe contener al menos 1 mayúscula, 1 minúscula, al menos 2 números y un carácter especial").custom(helpersUsuario.validarPassword),
-    validarCampos
-],httpUsuario.registroUsuario)
+    check("rol", "Rol no válido: admin, instructor, bodega").custom(
+      helpersUsuario.validarRol
+    ),
+    check(
+      "password",
+      "La contraseña debe contener al menos 1 mayúscula, 1 minúscula, al menos 2 números y un carácter especial"
+    ).custom(helpersUsuario.validarPassword),
+    validarCampos,
+  ],
+  httpUsuario.registroUsuario
+);
 
-router.post("/login",[
-    check('identificacion',"Digite su identificacion").not().isEmpty(),
-    check('password', "Digite la contraseña").not().isEmpty(),
-], httpUsuario.login)
+router.post(
+  "/login",
+  [
+    check("identificacion", "Digite su identificacion").not().isEmpty(),
+    check("password", "Digite la contraseña").not().isEmpty(),
+  ],
+  httpUsuario.login
+);
 
-router.post('/recuperar-password', httpUsuario.recuperarPassword)
+router.post("/recuperar-password", httpUsuario.recuperarPassword);
 
-router.put('/cambioPassword/:id',[
+//Put
+router.put(
+  "/cambioPassword/:id",
+  [
     validarJWT,
-    check('id', "Digite el id").not().isEmpty(),
-    check('id', "No es mongo id").isMongoId(),
-    check('password', "Digite la contraseña").not().isEmpty(),
-    check('newPassword', "Digite la nueva contraseña").not().isEmpty()
-],httpUsuario.putCambioPassword)
+    check("id", "Digite el id").not().isEmpty(),
+    check("id", "No es mongo id").isMongoId(),
+    check("password", "Digite la contraseña").not().isEmpty(),
+    check("newPassword", "Digite la nueva contraseña").not().isEmpty(),
+  ],
+  httpUsuario.putCambioPassword
+);
 
-router.get('/logOut',[
-    validarJWT
-],httpUsuario.logOut)
-
-router.get('/all', [
+router.put(
+  "/editar/:id",
+  [
     validarJWT,
-],httpUsuario.getAll)
-export default router
+    check("id", "Ingrese una id").not().isEmpty(),
+    check("id", "Id no válida").isMongoId(),
+    check("id").custom(helpersUsuario.existeHolderById),
+    check("nombre", "Digite el nombre").not().isEmpty(),
+    check("identificacion", "Digite la identificacion").not().isEmpty(),
+    check("correo", "Digite el correo").not().isEmpty(),
+    check("correo", "Digite el correo").isEmail(),
+    check("telefono", "Digite el telefono").not().isEmpty(),
+    check("rol", "Digite el codigo del rol").not().isEmpty(),
+    check("rol", "Rol no válido: admin, instructor, bodega").custom(
+      helpersUsuario.validarRol
+    ),
+    validarCampos,
+  ],
+  httpUsuario.editarUsuario
+);
+
+router.get("/logOut", [validarJWT], httpUsuario.logOut);
+export default router;
