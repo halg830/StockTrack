68c35f47dc3f7458d9dad428769a531bf89b5979
halg830 <halg830@gmail.com>
2024-04-02 07:46:45 -0500

    p3

--- index.js
+++ index.js
@@ -29,12 +29,6 @@ app.use(express.json());
 app.use(bodyParser.json());
 app.use(cors());
 app.use(express.static('public'))
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-  next();
-});
 app.use("/api/area", area)
 app.use("/api/contrato", contrato)
 app.use("/api/dependencia", dependencia)
